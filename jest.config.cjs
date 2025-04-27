76fae1b65c74f2f41f7c297d3d0a8268d2fa8f24
blood <asteroid.xyz.1000@gmail.com>
2025-04-27 14:43:16 +0900

    テスト周りのモジュール変更

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,8 +1,10 @@
-// jest.config.ts
-export default {
+module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
   clearMocks: true,
   coverageProvider: 'v8',
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { useESM: true }],
+  },
 }
